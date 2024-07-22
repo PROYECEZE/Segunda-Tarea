68b0cca2cc34015b442e5d7d57b2034c08bd7937
Ezequiel Blanco Mercado <ezeblamer@outlook.es>
2024-07-22 18:00:45 -0500

    continuation of main and corrections of the main

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let slideIndex = 0;
 
 function moveSlide(n) {
-  const slides = document.querySelectorAll('.carousel-item');
+  const slides = document.querySelectorAll('.carousel__item');
   const totalSlides = slides.length;
 
   slideIndex += n;
@@ -13,26 +13,67 @@ function moveSlide(n) {
   }
 
   const offset = -slideIndex * 773; 
-  document.querySelector('.carousel-img').style.transform = `translateX(${offset}px)`;
+  document.querySelector('.carousel__img').style.transform = `translateX(${offset}px)`;
 
   updateDots();
 }
 
-/*indicadores*/
 function currentSlide(n) {
   slideIndex = n;
   const offset = -slideIndex * 773;
-  document.querySelector('.carousel-img').style.transform = `translateX(${offset}px)`;
+  document.querySelector('.carousel__img').style.transform = `translateX(${offset}px)`;
 
   updateDots();
 }
+
+// indicators //
 
 function updateDots() {
   const dots = document.querySelectorAll('.dot');
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[slideIndex].classList.add('active');
+  dots.forEach((dot, index) => {
+    dot.classList.toggle('active', index === slideIndex);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  updateDots();
+  document.querySelector('.prev').addEventListener('click', () => moveSlide(-1));
+  document.querySelector('.next').addEventListener('click', () => moveSlide(1));
+
+  document.querySelectorAll('.js-go-to-slide').forEach(button => {
+    button.addEventListener('click', (event) => {
+      const slideIndex = parseInt(event.target.dataset.slide);
+      currentSlide(slideIndex);
+    });
+  });
+
+  updateDots(); 
+});
+
+// Width of each card plus gap
+
+document.addEventListener('DOMContentLoaded', function() {
+  const slides = document.querySelector('.categories__products-shoppings');
+  const slideWidth = document.querySelector('.card__baner').offsetWidth + 24.98; 
+  let currentPosition = 0;
+
+  document.querySelectorAll('.js-moved-slide').forEach(button => {
+    button.addEventListener('click', function() {
+      const direction = parseInt(this.getAttribute('data-direction'));
+      const maxPosition = (slides.children.length - 4) * slideWidth;
+
+      if (direction === -1) {
+        currentPosition -= slideWidth;
+        if (currentPosition < 0) {
+          currentPosition = maxPosition;
+        }
+      } else {
+        currentPosition += slideWidth;
+        if (currentPosition > maxPosition) {
+          currentPosition = 0;
+        }
+      }
+
+      slides.style.transform = `translateX(-${currentPosition}px)`;
+    });
+  });
 });
